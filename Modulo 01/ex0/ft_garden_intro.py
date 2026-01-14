4c0a775bd8729e1d2dd3db83280f79fd37c6740a
Tainna Almeida Braganca <taalmeid@c2r2s2.42porto.com>
2026-01-14 18:26:14 +0000

    42 today exercises

---
+++ Modulo 01/ex0/ft_garden_intro.py
@@ -0,0 +1,15 @@
+def ft_garden_intro() -> None:
+    print("=== Welcome to My Garden ===")
+    
+    name = "Rose"
+    height = "25cm"
+    age = "30 days"
+    
+    print("Plant:", name)
+    print("Height:", height)
+    print("Age:", age)
+
+    print("=== End of Program ===")
+
+if __name__ == "__main__":
+    ft_garden_intro()
