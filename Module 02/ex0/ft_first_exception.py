1955e5a0089194bc172337112e4fc4e20b339e21
Tainna Almeida Braganca <taalmeid@c1r16s7.42porto.com>
2026-01-20 21:48:05 +0000

    exercises from today 42

---
+++ Module 02/ex0/ft_first_exception.py
@@ -0,0 +1,8 @@
+
+def check_temperature(temp_str):
+    try:
+        numero = int(temp_str)
+        return(numero)
+    except ValueError:
+        print(f"{temp_str} is not a valid number")
+    
