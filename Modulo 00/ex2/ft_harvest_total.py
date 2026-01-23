3f09a60fef72679319f4254a7a73cf18b74bfe76
tainna <64937004+tainna@users.noreply.github.com>
2026-01-23 18:30:36 +0000

    Update ft_harvest_total.py

--- Modulo 00/ex2/ft_harvest_total.py
+++ Modulo 00/ex2/ft_harvest_total.py
@@ -2,6 +2,6 @@ def ft_harvest_total():
 
     day1 = int(input("Enter day 1 harvest: "))
     day2 = int(input("Enter day 2 harvest: "))
-    day3 = int(input("Enter day 1 harvest: "))
+    day3 = int(input("Enter day 3 harvest: "))
     harvest_total = day1+day2+day3
     print("Total:", harvest_total)
