3d961532fc0b89711bd3778888a00d9dd6fcd931
Tainna Almeida Braganca <taalmeid@c2r1s10.42porto.com>
2026-03-10 16:02:11 +0000

    new module

---
+++ Module 05/ex0/stream_processor.py
@@ -0,0 +1,68 @@
+from abc import ABC, abstractmethod
+from typing import Any, List, Dict, Union, Optional
+
+
+class DataProcessor(ABC):
+
+    @abstractmethod
+    def validate(self, data: Any) -> bool:
+        pass
+
+    @abstractmethod
+    def process(self, data: Any) -> str:
+        pass
+
+    def format_output(self, result: str) -> str:
+        return f"Output: {result}"
+
+
+class NumericProcessor(DataProcessor):
+
+    def validate(self, data: Any) -> bool:
+
+        if not isinstance(data, list) or len(data) == 0:
+            return False
+        for item in data:
+            if not isinstance(item, (int, float)):
+                return False
+
+        return True
+
+    def process(self, data: Any) -> bool:
+
+        try:
+            d_size = len(data)
+            d_sum = sum(data)
+            d_med = d_sum / d_size
+
+            return (
+                f"Processed {d_size} numeric values, "
+                f"sum={d_sum}, avg={d_med:.1f}"
+            )
+        except ValueError as error:
+            return (f"Error processing numeric data: {error}")
+
+
+class TextProcessor(DataProcessor):
+
+    def validate(self, data: Any) -> bool:
+        if not isinstance(data, str) or len(data) == 0:
+            return False
+        return True
+
+    def process(self, data: Any) -> str:
+
+        try:
+            len_d = len(data)
+            sum_d = len(data.split())
+            # print(f"Processing data: {d_str}")
+            # print("Validation: Text data verified")
+            return (f"Processed text: {len_d} characters, {sum_d} words")
+        except ValueError as error:
+            return (f"Error processing data: {error}")
+
+
+class LogProcessor(DataProcessor):
+
+    def validate(self, data) -> bool:
+        if not isinstance(data, )
