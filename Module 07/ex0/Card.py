176fdcdf866f672a4bf9f22b4ebc178ca3176474
tainna <tainnaab@gmail.com>
2026-03-24 23:43:35 +0100

    Mod07 start

---
+++ Module 07/ex0/Card.py
@@ -0,0 +1,14 @@
+from abc import ABC, abstractmethod
+
+
+def __init__(self, name: str, cost: int, rarity: str):
+
+
+def play(self, game_state: dict) -> dict:
+    
+
+def get_card_info(self) -> dict:
+    
+
+def is_playable(self, available_mana: int) -> bool:
+    pass
